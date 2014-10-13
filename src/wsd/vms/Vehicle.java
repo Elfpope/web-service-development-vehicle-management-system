1f9c3fa0b65a3646415dd07c827654e92e0f2875
alexfengau <alexfengau@gmail.com>
2014-10-13 11:07:44 +0000

    Create user and vehicle class plus xml files

---
+++ src/wsd/vms/Vehicle.java
@@ -0,0 +1,16 @@
+package wsd.vms;
+
+import java.io.Serializable;
+
+public class Vehicle implements Serializable {
+	private String registrationNum;
+	private Type vehicleType;
+	private String make;
+	private String model;
+	private int odometer;
+	
+	public Vehicle() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
