0fb63960519423b0fdc775d4f20019ba837dbd97
rebecca-ao@hotmail.com <rebecca-ao@hotmail.com@571bf6e2-e085-222c-3004-3ea1fe3e409a>
2014-10-18 01:48:12 +0000

    Jason Commit: Added XML load for vehicles, changed trips date/time to int

--- src/wsd/vms/Vehicles.java
+++ src/wsd/vms/Vehicles.java
@@ -3,7 +3,14 @@ package wsd.vms;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "vehicles")
 public class Vehicles implements Serializable {
+	@XmlElement(name = "vehicle")
 	private ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>();
 	
 	public Vehicles() {	}
