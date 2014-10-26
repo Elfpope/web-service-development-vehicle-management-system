993570dbe84f172ccb0495e8f454fd7e78e81c35
alexfengau@gmail.com <alexfengau@gmail.com@571bf6e2-e085-222c-3004-3ea1fe3e409a>
2014-10-26 22:20:51 +0000

    First of all, no functional changes are made in this commit:
    1. Add in some comments for explanation into Dao interfaces
    2. Add in some comments for explanation into XMLService class
    3. Add comments into index.jsp about keeping only one VSM instance 
    4. Add root using-schema attributes for users.xml and vehicles.xml for justifying its function but they are inactivated  
    5. Change date and time types in vehicles.xsd as using string for date/time is insufficient as per requirement
    6. Add 3 users into users.xml based on vehicles.xml to keep users and vehicles data consistant

--- src/wsd/vms/IVehiclesDao.java
+++ src/wsd/vms/IVehiclesDao.java
@@ -2,18 +2,28 @@ package wsd.vms;
 
 import java.util.ArrayList;
 
+/**
+ * Vehicles Dao interface deals with all vehicles data CRUD.
+ */
+
 public interface IVehiclesDao {
 	
+	//retrieve all vehicles from the data source
 	 public Vehicles getVehicles();
 	 
+	//adds a new vehicle into the data source
 	 public void addVehicle(Vehicle vehicle);
 	 
+	//retrieve all trips from the data source
 	 public ArrayList<Trip> getTrips();
 	 
+	//retrieve the trips object from the data source as per criteria
 	 public ArrayList<Trip> getTrips(String vehicleRego, int startDate, String keyword);
 	 
+	//adds a new trip into the data source
 	 public void addTrip(Trip trip);
 	 
+	 //flap a trip as deleted
 	 public boolean deleteTrip(int tripId, int userId);
 
 }
