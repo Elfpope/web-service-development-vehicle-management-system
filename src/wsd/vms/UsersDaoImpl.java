09b21d6695bfbb9a0973c89b24490efb5c5f022e
rebecca-ao@hotmail.com <rebecca-ao@hotmail.com@571bf6e2-e085-222c-3004-3ea1fe3e409a>
2014-10-26 09:54:01 +0000

    Jason commit: backend validation

--- src/wsd/vms/UsersDaoImpl.java
+++ src/wsd/vms/UsersDaoImpl.java
@@ -7,15 +7,16 @@ import java.io.Serializable;
 import javax.xml.bind.JAXBException;
 
 public class UsersDaoImpl implements IUsersDao, Serializable {
-	
+
 	private XMLService xmlService;
 	private Users users;
 
 	public UsersDaoImpl() {
 		xmlService = new XMLService();
 	}
-	
-	public UsersDaoImpl(XMLService xmlService) throws JAXBException, IOException {
+
+	public UsersDaoImpl(XMLService xmlService) throws JAXBException,
+			IOException {
 		this.xmlService = xmlService;
 		users = xmlService.unmarshallUsers();
 	}
@@ -26,7 +27,7 @@ public class UsersDaoImpl implements IUsersDao, Serializable {
 	}
 
 	@Override
-	//retrieve a singel user as per credential 
+	// retrieve a singel user as per credential
 	public User getUser(String email, String password) {
 		return users.login(email, password);
 	}
@@ -45,11 +46,11 @@ public class UsersDaoImpl implements IUsersDao, Serializable {
 	}
 
 	@Override
-	public void setUsers(Users users){
+	public void setUsers(Users users) {
 		this.users = users;
 		try {
 			xmlService.marshallUsers(users);
-		} catch (FileNotFoundException e ) {
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (JAXBException e) {
 			e.printStackTrace();
@@ -60,9 +61,57 @@ public class UsersDaoImpl implements IUsersDao, Serializable {
 	public void addUser(User user) {
 		int id = users.getUsers().size() + 1;
 		user.setId(id);
-		users.addUser(user);
-
-		setUsers(users);
+		if (checkUser(user)) {
+			users.addUser(user);
+			setUsers(users);
+		}
 	}
 
+	public boolean checkUser(User user) {
+		if ((isValidEmail(user.getEmail()))
+				&& (isValidName(user.getFirstName()))
+				&& (isValidName(user.getLastName()))
+				&& (isValidPassword(user.getPassword()))
+				&& (isCorrectRole(user.getRole())) && (isValidId(user.getId()))) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean isValidEmail(String email) {
+		String regex = ".*@.*\\.(com|gov|org|net|edu).*";
+		if (email.matches(regex))
+			return true;
+		return false;
+	}
+
+	// Validation Rule: Must contain valid letters
+	public boolean isValidName(String name) {
+		String regex = "[a-z]*";
+		if (name.matches(regex))
+			return true;
+		return false;
+	}
+
+	// Validation Rule: any lower or upper case letter or any digits
+	public boolean isValidPassword(String password) {
+		String regex = "([a-zA-Z0-9]){4,16}";
+		if (password.matches(regex))
+			return true;
+		return false;
+	}
+
+	public boolean isCorrectRole(String role) {
+		if (role.equals("Driver") || role.equals("Administrator")) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean isValidId(int id) {
+
+		if (id >= 0)
+			return true;
+		return false;
+	}
 }
